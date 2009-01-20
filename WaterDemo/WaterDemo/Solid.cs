53bfd9824e2fe663bec1c8d9e10b906e5b6f28d4
adn1646 <adn1646@d4877372-545d-0410-ac41-8c8efc50645a>
2009-01-20 02:56:14 +0000

    Check out water demo ;)

---
+++ WaterDemo/WaterDemo/Solid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WaterDemo
+{
+	class Solid
+	{
+		internal Vector3 Position = Vector3.Zero;
+		internal Vector3 Velocity = Vector3.Zero;
+
+		internal Vector3 VelocityEval = Vector3.Zero;
+
+		internal Vector3 Acceleration = Vector3.Zero;
+
+		internal float radius = 2;
+
+		internal BoundingSphere BoundingSphere
+		{
+			get
+			{
+				return new BoundingSphere(Position, radius);
+			}
+		}
+
+		public Solid(Vector3 startPosition)
+		{
+			StartPosition = startPosition;
+			LastPosition = startPosition;
+			Position = startPosition;
+		}
+
+		internal Vector3 StartPosition = Vector3.Zero;
+		internal Vector3 LastPosition = Vector3.Zero;
+	}
+}
